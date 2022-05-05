e415be3d86c523a81928378e3d6f83f301a3b6d5
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-05-05 17:35:33 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo"
+git clone https://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@github.com/JoYaL-TG/Superman.git
+cd Superman
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
